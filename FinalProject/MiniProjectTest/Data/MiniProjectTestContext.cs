124966c30af2a2f85e2380e8f3ea4deb723fbc3c
kondareddy-janapala <kondareddy.janapala0143@gmail.com>
2022-07-09 12:58:57 +0530

    final project

---
+++ FinalProject/MiniProjectTest/Data/MiniProjectTestContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MiniProject.Models;
+
+namespace MiniProjectTest.Data
+{
+    public class MiniProjectTestContext : DbContext
+    {
+        public MiniProjectTestContext (DbContextOptions<MiniProjectTestContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MiniProject.Models.Product> Product { get; set; }
+    }
+}
---
+++ FinalProject/MiniProjectTest/Model/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniProject.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        [Required]
+        public string ProductName { get; set; }
+        [Range(1.0, Double.PositiveInfinity)]
+        public double Price { get; set; }
+        public string Brand { get; set; }
+        public DateTime ManufactureDate { get; set; }
+        public DateTime ExpirationDate { get; set; }
+
+
+    }
+}
